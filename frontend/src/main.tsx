40c713b44eda74a04aa4889c49c5133b97278739
Cleon Kalea <totivonest@gmail.com>
2026-05-22 19:10:19 +0800

    chore: Hard reset frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,22 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false, // avoid refetching data when moving through the app
-      retry: 1, // try fetching data once more if it fails
-      staleTime: 1000 * 60 * 5, // Refresh data every 5 minutes
-    },
-  },
-})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 )
